4e1c15ce8d2d7c09325d85e6107088b3aa3b4c48
yonggyo1981 <yonggyo1981@gmail.com>
2021-07-26 16:06:02 +0900

    .

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express(); // express 객체가 생성 
 
+app.set('PORT', 8000);
+
+/** 
+	가장상단에 있는 미들웨어 전용 라우터 
+	- 모든 요청에 유입
+	
+*/
+app.use((req, res, next) => {
+	console.log("공통 라우터!");
+	req.shareData = "공유데이터"; 
+	next();
+});
+
 
 // GET - / 라우터 
 app.get("/", (req, res) => {
+	console.log("/", req.shareData);
 	/**
 	req - Request 
 	res - Response 
@@ -15,8 +29,8 @@ app.get("/", (req, res) => {
 });
 
 
-
 app.get("/test", (req, res, next) => {
+	console.log("/test", req.shareData);
 	//return res.send("<h1>테스트 라우터</h1>");
 	console.log("0번째 미들웨어");
 	next(); // 다음 해당하는 라우터의 첫번째 미들웨어로 이동 
@@ -31,9 +45,20 @@ app.get("/test", (req, res, next) /* 1번째 미들웨어 */ => {
 }, (req, res) /* 3번째 미들웨어 */ => {
 	console.log("3번째 미들웨어");
 	
-	return res.send("");
+	return res.send("/test 라우터");
 });
 
-app.listen(3000, () => {
-	console.log("3000번 포트에서 서버 대기중...");
+/** 
+ 가장 하단에 라우터 - 모든 요청, 모든 메서드 use 
+						  - 없는 페이지 처리 라우터 
+						  - 상태코드를 404로해서 없는페이지라고 브라우저에 전달 
+*/
+app.use((req, res, next) => {
+	//return res.send("<h1>가장 하단에 있는 모든 요청 처리 라우터</h1>");
+	return res.status(404).send("현재 접속한 " + req.url + "은 없는 페이지 입니다...");
+});
+
+
+app.listen(app.get('PORT'), () => {
+	console.log(app.get('PORT'), "번 포트에서 서버 대기중...");
 });
